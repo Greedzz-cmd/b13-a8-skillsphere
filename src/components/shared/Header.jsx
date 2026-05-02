80e86cf2cf194f272123490bfeb9f1372975ed1c
greed <rakib123kib.ra@gmail.com>
2026-05-02 13:33:06 +0600

    project initiated and added header and navbar

---
+++ src/components/shared/Header.jsx
@@ -0,0 +1,79 @@
+import Link from "next/link";
+import logo from "@/../public/logo.png";
+import Image from "next/image";
+
+const Header = () => {
+  const links = (
+    <>
+      <Link href="/" className="relative inline-block group cursor-pointer">
+        Home
+        <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
+      </Link>
+      <Link
+        href="/courses"
+        className="relative inline-block group cursor-pointer "
+      >
+        Courses
+        <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
+      </Link>
+      <Link
+        href="/profile"
+        className="relative inline-block group cursor-pointer"
+      >
+        My Profile
+        <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
+      </Link>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
+        <Link href="/" className="btn btn-ghost text-xl">
+          <Image src={logo} width={40} height={40} alt="logo"></Image>
+          <p>SkillSphere</p>
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 space-x-8 text-gray-600">
+          {links}
+        </ul>
+      </div>
+      <div className="navbar-end space-x-2">
+        <Link href="/signin" className="btn btn-ghost ">
+          Sign In
+        </Link>
+        <Link href="/signup" className="btn text-white bg-black rounded-lg">
+          Join for Free
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/shared/Hero.jsx
@@ -0,0 +1,47 @@
+const Hero = () => {
+  return (
+    <section className="bg-gray-50 py-24 text-center px-4">
+      <p className="text-sm text-[#ccbe8e] font-medium mb-3 bg-[#f1f3e7] rounded-full badge">
+        PREMIUM LEARNING PLATFORM
+      </p>
+
+      <h1 className="text-5xl md:text-6xl font-bold leading-tight">
+        Upgrade Your Skills <br />
+        With <span className="text-[#C77A52]">Confidence</span>
+      </h1>
+
+      <p className="text-gray-500 mt-4 max-w-xl mx-auto">
+        Learn Web Development, Design, and Marketing from industry experts at
+        your own pace.
+      </p>
+
+      <div className="flex justify-center gap-4 mt-8 flex-wrap">
+        <button className="btn rounded-full bg-black text-white">
+          Start Learning
+        </button>
+
+        <button className="btn btn-ghost rounded-full">Explore Courses</button>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 text-center mt-16">
+        <div>
+          <h2 className="text-xl font-bold">10k+</h2>
+          <p className="text-gray-500 text-sm">Students</p>
+        </div>
+        <div>
+          <h2 className="text-xl font-bold">500+</h2>
+          <p className="text-gray-500 text-sm">Courses</p>
+        </div>
+        <div>
+          <h2 className="text-xl font-bold">4.9/5</h2>
+          <p className="text-gray-500 text-sm">Rating</p>
+        </div>
+        <div>
+          <h2 className="text-xl font-bold">24/7</h2>
+          <p className="text-gray-500 text-sm">Support</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
